1af7c1bab3f361e9f219075f88200a2d2257bb0d
TB <tatianabertazoli@gmail.com>
2022-09-08 16:50:27 -0500

    created populate table function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,30 @@
-var idDate = document.getElementById("currentDay");
-var todaysDate = moment().format('dddd, MMMM Do YYYY');
+$(document).ready(function () {
+    populateTable();
+});
 
-$(idDate).append(todaysDate);
+// Today's date function
+var displayDate = $("#currentDay");
+var todaysDate = moment().format('dddd, MMMM Do YYYY');
+$(displayDate).append(todaysDate);
+
+var container = $("#container");
+
+
+//function to create the table
+function populateTable() {
+    for (var i = 0; i <= 24; i++) {
+        var rowElement = document.createElement("div");
+        rowElement.classList.add("row");
+        container.append(rowElement);
+        for (var generateColumn = 0; generateColumn < 3; generateColumn++) {
+            var columnElement = document.createElement("div");
+            columnElement.classList.add("col-sm");
+            rowElement.append(columnElement);
+        }
+    }
+}
+
+
+
+
+
