d6b0a756ce1d3400c4aafdb80522c5407557db7c
TB <tatianabertazoli@gmail.com>
2022-09-07 20:01:43 -0500

    changed date to Jquery style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,4 @@
-function generateDate() {
-    var idDate = document.getElementById("currentDay");
-    var todaysDate = moment().format('dddd, MMMM Do YYYY');
-    idDate.textContent = todaysDate;    
-};
-generateDate();d
+var idDate = document.getElementById("currentDay");
+var todaysDate = moment().format('dddd, MMMM Do YYYY');
 
-
+$(idDate).append(todaysDate);
