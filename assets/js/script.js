08837a804fdd553a63346805bd7dd13b8236c909
TB <tatianabertazoli@gmail.com>
2022-09-10 17:47:57 -0500

    added change color function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-// $(document).ready(function () {
-//     populateTable();
-// });
+$(document).ready(() => {
+    changeColor();
+})
 
 // Today's date function
 var displayDate = $("#currentDay");
@@ -8,7 +8,21 @@ var todaysDate = moment().format('dddd, MMMM Do YYYY');
 $(displayDate).append(todaysDate);
 
 var container = $("#container");
-var setTimer = moment().format("HH a");
-console.log(setTimer);
+var actualHour = moment().format("H");
+
+// function change color
+function changeColor() {
+    $(".row.content").each(function (index) {
+        var className = "";
+        if (index + 8 === actualHour) {
+            className = "present";            
+        } else if (index + 8 < actualHour) {
+           className = "past"
+        } else {
+          className = "future"
+        }
+        $(this).children().eq(1).addClass(className);        
+    });
+}
 
 
