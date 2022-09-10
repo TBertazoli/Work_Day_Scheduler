34138b0eda550dd34d6b54354a5bf9de7dec4587
TB <tatianabertazoli@gmail.com>
2022-09-10 11:54:50 -0500

    added hour format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
-    populateTable();
-});
+// $(document).ready(function () {
+//     populateTable();
+// });
 
 // Today's date function
 var displayDate = $("#currentDay");
@@ -8,23 +8,7 @@ var todaysDate = moment().format('dddd, MMMM Do YYYY');
 $(displayDate).append(todaysDate);
 
 var container = $("#container");
-
-
-//function to create the table
-function populateTable() {
-    for (var i = 0; i <= 24; i++) {
-        var rowElement = document.createElement("div");
-        rowElement.classList.add("row");
-        container.append(rowElement);
-        for (var generateColumn = 0; generateColumn < 3; generateColumn++) {
-            var columnElement = document.createElement("div");
-            columnElement.classList.add("col-sm");
-            rowElement.append(columnElement);
-        }
-    }
-}
-
-
-
+var setTimer = moment().format("HH a");
+console.log(setTimer);
 
 
